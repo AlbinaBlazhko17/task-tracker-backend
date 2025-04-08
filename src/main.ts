a1fc8f1fc76a0ea29591642809b144f516d4cfcd
Albina Blazhko <46962291+AlbinaBlazhko17@users.noreply.github.com>
2025-04-08 16:19:21 +0300

    feat: add aliases

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
 import { HttpAdapterHost, NestFactory } from '@nestjs/core'
-import { AppModule } from './app/app.module'
+import { AppModule } from '@app/app.module'
 import * as cookieParser from 'cookie-parser'
-import { AppExceptionsFilter } from './app/app-exceptions.filter'
-import { EmojiLogger } from './app/logger.service'
+import { AppExceptionsFilter } from '@app/app-exceptions.filter'
+import { EmojiLogger } from '@app/logger.service'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
