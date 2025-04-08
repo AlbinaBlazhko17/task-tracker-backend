0cd483856c27a995659f9adc88c545d84d9440a6
Albina Blazhko <46962291+AlbinaBlazhko17@users.noreply.github.com>
2025-04-08 15:55:49 +0300

    fix: add back logger

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,9 @@ import { AppExceptionsFilter } from './app/app-exceptions.filter'
 import { EmojiLogger } from './app/logger.service'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule)
+  const app = await NestFactory.create(AppModule, {
+    logger: new EmojiLogger()
+  })
 
   app.setGlobalPrefix('/api')
   app.use(cookieParser())
