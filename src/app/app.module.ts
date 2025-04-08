8c9be19abd875f121b0f6a0c135d6f9c793760cd
Albina Blazhko <46962291+AlbinaBlazhko17@users.noreply.github.com>
2025-04-08 15:57:32 +0300

    feat: add global core module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,14 @@ import { ConfigModule } from '@nestjs/config'
 import appConfig from './config/app.config'
 import { AuthModule } from './modules/auth/auth.module'
 import { UserModule } from './modules/user/user.module'
+import { CoreModule } from './core/core.module'
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       load: [appConfig]
     }),
+    CoreModule,
     UserModule,
     AuthModule
   ]
