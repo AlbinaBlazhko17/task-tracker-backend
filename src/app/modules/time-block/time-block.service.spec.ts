36fa8d66d7a53147de524169856da34e59f36a51
Albina Blazhko <46962291+AlbinaBlazhko17@users.noreply.github.com>
2025-05-08 12:21:14 +0300

    feat: add tests for time-block service

--- src/app/modules/time-block/time-block.service.spec.ts
+++ src/app/modules/time-block/time-block.service.spec.ts
@@ -1,19 +1,145 @@
 import { Test, TestingModule } from '@nestjs/testing'
 
+import { TimeBlockDto } from './dto/time-block.dto'
 import { TimeBlockService } from './time-block.service'
+import { PrismaService } from '@/core/prisma/prisma.service'
 
 describe('TimeBlockService', () => {
   let service: TimeBlockService
+  let prisma: PrismaService
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [TimeBlockService]
+      providers: [
+        TimeBlockService,
+        {
+          provide: PrismaService,
+          useValue: new PrismaService()
+        }
+      ]
     }).compile()
 
     service = module.get<TimeBlockService>(TimeBlockService)
+    prisma = module.get<PrismaService>(PrismaService)
   })
 
   it('should be defined', () => {
     expect(service).toBeDefined()
+  })
+
+  describe('findAll', () => {
+    it('should return all time blocks for a user', async () => {
+      const mockTimeBlocks = [
+        {
+          id: '1',
+          userId: 'user1',
+          order: 1,
+          name: 'Block 1',
+          color: 'red',
+          duration: 30,
+          createdAt: new Date(),
+          updatedAt: new Date()
+        }
+      ]
+      jest.spyOn(prisma.timeBlock, 'findMany').mockResolvedValue(mockTimeBlocks)
+
+      const result = await service.findAll('user1')
+      expect(result).toEqual(mockTimeBlocks)
+      expect(prisma.timeBlock.findMany).toHaveBeenCalledWith({
+        where: { userId: 'user1' },
+        orderBy: { order: 'asc' }
+      })
+    })
+  })
+
+  describe('create', () => {
+    it('should create a new time block', async () => {
+      const timeBlockDto: TimeBlockDto = {
+        name: 'Test Block',
+        order: 1,
+        color: 'red',
+        duration: 30
+      }
+      const mockTimeBlock = {
+        id: '1',
+        ...timeBlockDto,
+        userId: 'user1',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }
+      jest.spyOn(prisma.timeBlock, 'create').mockResolvedValue(mockTimeBlock)
+
+      const result = await service.create(timeBlockDto, 'user1')
+      expect(result).toEqual(mockTimeBlock)
+      expect(prisma.timeBlock.create).toHaveBeenCalledWith({
+        data: {
+          ...timeBlockDto,
+          user: { connect: { id: 'user1' } }
+        }
+      })
+    })
+  })
+
+  describe('update', () => {
+    it('should update a time block', async () => {
+      const timeBlockDto: Partial<TimeBlockDto> = { name: 'Updated Block' }
+      const mockUpdatedBlock = {
+        id: '1',
+        name: 'Updated Block',
+        userId: 'user1',
+        order: 1,
+        color: 'red',
+        duration: 30,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }
+      jest.spyOn(prisma.timeBlock, 'update').mockResolvedValue(mockUpdatedBlock)
+
+      const result = await service.update('1', timeBlockDto, 'user1')
+      expect(result).toEqual(mockUpdatedBlock)
+      expect(prisma.timeBlock.update).toHaveBeenCalledWith({
+        where: { userId: 'user1', id: '1' },
+        data: timeBlockDto
+      })
+    })
+  })
+
+  describe('delete', () => {
+    it('should delete a time block', async () => {
+      const mockDeletedBlock = {
+        id: '1',
+        userId: 'user1',
+        name: 'Block 1',
+        order: 1,
+        color: 'red',
+        duration: 30,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }
+      jest.spyOn(prisma.timeBlock, 'delete').mockResolvedValue(mockDeletedBlock)
+
+      const result = await service.delete('1', 'user1')
+      expect(result).toEqual(mockDeletedBlock)
+      expect(prisma.timeBlock.delete).toHaveBeenCalledWith({
+        where: { userId: 'user1', id: '1' }
+      })
+    })
+  })
+
+  describe('updateOrder', () => {
+    it('should update the order of time blocks', async () => {
+      const ids = ['1', '2', '3']
+      const mockTransactionResult = [
+        { id: '1', order: 0 },
+        { id: '2', order: 1 },
+        { id: '3', order: 2 }
+      ]
+      jest
+        .spyOn(prisma, '$transaction')
+        .mockResolvedValue(mockTransactionResult)
+
+      const result = await service.updateOrder(ids)
+      expect(result).toEqual(mockTransactionResult)
+    })
   })
 })
